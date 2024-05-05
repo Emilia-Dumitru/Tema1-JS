31aa8a6aca2cff4461bf371f73e04a9df04b0875
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-05-05 18:49:37 +0300

    primafunctie

---
+++ ex1.js
@@ -0,0 +1,16 @@
+//How to check if a number is divisible by 10
+//Methot 1
+
+function divisiblenumber (number, a) {
+    if (number % a === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+};
+console.log(divisiblenumber(100,10));
+console.log(divisiblenumber(23,10));
+
+//Method 2
+
