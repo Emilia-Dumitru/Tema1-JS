aaaf249235798a8f357cae4086809f6338a3dc50
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-05-05 19:58:04 +0300

    ex3 inceput

---
+++ ex3.js
@@ -0,0 +1,5 @@
+/*Veți crea o funcție care va returna un șir de caractere (string) 
+care va reprezenta numele unei zile a săptămânii 
+în raport cu numărul acesteia pe parcursul unei săptămâni.
+
+*/
